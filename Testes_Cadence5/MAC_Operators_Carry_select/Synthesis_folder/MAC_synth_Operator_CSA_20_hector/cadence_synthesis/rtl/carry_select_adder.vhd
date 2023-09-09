5734faf6835a4d5e559b292338922175bc7e6e00
Luis Antonio Spader Simon <luisspaders@gmail.com>
2023-09-09 18:59:50 -0300

    CSA comeback literal values

--- Testes_Cadence5/MAC_Operators_Carry_select/Synthesis_folder/MAC_synth_Operator_CSA_20_hector/cadence_synthesis/rtl/carry_select_adder.vhd
+++ Testes_Cadence5/MAC_Operators_Carry_select/Synthesis_folder/MAC_synth_Operator_CSA_20_hector/cadence_synthesis/rtl/carry_select_adder.vhd
@@ -7,50 +7,50 @@ ENTITY carry_select_adder IS
         bits : POSITIVE := 16
     );
     PORT (
-        a, b : IN UNSIGNED(bits - 1 DOWNTO 0);
-        res  : OUT UNSIGNED(bits - 1 DOWNTO 0)
+        a, b : IN UNSIGNED(15 DOWNTO 0);
+        res  : OUT UNSIGNED(15 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE rtl OF carry_select_adder IS
     -- SIGNAL carry_final                                             : UNSIGNED(bits DOWNTO 0);
     -- SIGNAL c_lsb, c0, c1                                           : UNSIGNED(bits/2 DOWNTO 0);
-    SIGNAL a_LSB, b_LSB, G_LSB                          : UNSIGNED((bits/2) DOWNTO 0);
-    SIGNAL a_MSB_0, b_MSB_0, a_MSB_1, b_MSB_1, G_0, G_1 : UNSIGNED((bits/2) + 1 DOWNTO 0);
+    SIGNAL a_LSB, b_LSB, G_LSB                          : UNSIGNED(8 DOWNTO 0);
+    -- SIGNAL a_MSB_0, b_MSB_0, a_MSB_1, b_MSB_1, G_0, G_1 : UNSIGNED(8 + 1 DOWNTO 0);
 
-    -- SIGNAL a_MSB_0, b_MSB_0, a_MSB_1, b_MSB_1, G_0, G_1 : UNSIGNED((bits/2) DOWNTO 0);
+    SIGNAL a_MSB_0, b_MSB_0, a_MSB_1, b_MSB_1, G_0, G_1 : UNSIGNED(8 DOWNTO 0);
 BEGIN
 
-    a_LSB                      <= '0' & a((bits/2) - 1 DOWNTO 0);
+    a_LSB             <= '0' & a(7 DOWNTO 0);
 
-    b_LSB                      <= '0' & b((bits/2) - 1 DOWNTO 0);
+    b_LSB             <= '0' & b(7 DOWNTO 0);
 
-    G_LSB((bits/2) DOWNTO 0)   <= a_LSB + b_LSB;
+    G_LSB(8 DOWNTO 0) <= a_LSB + b_LSB;
 
-    res((bits/2) - 1 DOWNTO 0) <= G_LSB((bits/2) - 1 DOWNTO 0);
+    res(7 DOWNTO 0)   <= G_LSB(7 DOWNTO 0);
 
-    a_MSB_0                    <= a(bits - 1 DOWNTO (bits/2)) & '0';
+    a_MSB_0           <= a(15 DOWNTO 8) & '0';
 
-    b_MSB_0                    <= b(bits - 1 DOWNTO (bits/2)) & '0';
+    b_MSB_0           <= b(15 DOWNTO 8) & '0';
 
-    a_MSB_1                    <= a(bits - 1 DOWNTO (bits/2)) & '1';
+    a_MSB_1           <= a(15 DOWNTO 8) & '1';
 
-    b_MSB_1                    <= b(bits - 1 DOWNTO (bits/2)) & '1';
+    b_MSB_1           <= b(15 DOWNTO 8) & '1';
 
-    G_0((bits/2) + 1 DOWNTO 0) <= a_MSB_0 + b_MSB_0;
+    -- G_0(8 + 1 DOWNTO 0) <= a_MSB_0 + b_MSB_0;
 
-    G_1((bits/2) + 1 DOWNTO 0) <= a_MSB_1 + b_MSB_1;
+    -- G_1(8 + 1 DOWNTO 0) <= a_MSB_1 + b_MSB_1;
 
-    res(15 DOWNTO 8)           <= G_0((bits/2) + 1 DOWNTO 2) WHEN G_LSB((bits/2)) = '0' ELSE
-    G_1((bits/2) + 1 DOWNTO 2);
+    -- res(15 DOWNTO 8)    <= G_0(8 + 1 DOWNTO 2) WHEN G_LSB(8) = '0' ELSE
+    -- G_1(8 + 1 DOWNTO 2);
 
     -- --------------------------------------------------------------------------------
-    -- G_0((bits/2) DOWNTO 0)        <= a_MSB_0 + b_MSB_0;
+    G_0(8 DOWNTO 0)   <= a_MSB_0 + b_MSB_0;
 
-    -- G_1((bits/2) DOWNTO 0)        <= a_MSB_1 + b_MSB_1;
+    G_1(8 DOWNTO 0)   <= a_MSB_1 + b_MSB_1;
 
-    -- res(bits - 1 DOWNTO (bits/2)) <= G_0((bits/2) DOWNTO 1) WHEN G_LSB((bits/2)) = '0' ELSE
-    -- G_1((bits/2) DOWNTO 1);
+    res(15 DOWNTO 8)  <= G_0(8 DOWNTO 1) WHEN G_LSB(8) = '0' ELSE
+    G_1(8 DOWNTO 1);
 
     -- --------------------------------------------------------------------------------
     -- c_lsb((BITS/2) DOWNTO 0)           <= a((BITS/2) - 1 DOWNTO 0) + b((BITS/2) - 1 DOWNTO 0);
@@ -59,7 +59,7 @@ BEGIN
 
     -- c1                                 <= a(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))) + b(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))) + '1';
 
-    -- carry_final((BITS/2) - 1 DOWNTO 0) <= c_lsb((BITS/2) - 1 DOWNTO 0); -- (bits/2) -1 downto 0
+    -- carry_final((BITS/2) - 1 DOWNTO 0) <= c_lsb((BITS/2) - 1 DOWNTO 0); -- 8 -1 downto 0
 
     -- IF_PROC : PROCESS (c_lsb(BITS/2))                                   -- 4  
     -- BEGIN
