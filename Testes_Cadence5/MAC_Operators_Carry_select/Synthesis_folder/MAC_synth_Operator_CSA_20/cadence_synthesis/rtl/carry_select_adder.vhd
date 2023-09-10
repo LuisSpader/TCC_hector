fc153b3ddc44916b0a723514348725cf82b68a97
Luis Antonio Spader Simon <luisspaders@gmail.com>
2023-09-09 22:45:53 -0300

    CSA meu fix

--- Testes_Cadence5/MAC_Operators_Carry_select/Synthesis_folder/MAC_synth_Operator_CSA_20/cadence_synthesis/rtl/carry_select_adder.vhd
+++ Testes_Cadence5/MAC_Operators_Carry_select/Synthesis_folder/MAC_synth_Operator_CSA_20/cadence_synthesis/rtl/carry_select_adder.vhd
@@ -18,41 +18,7 @@ ARCHITECTURE rtl OF carry_select_adder IS
     SIGNAL c0, c1         : UNSIGNED(bits/2 - 1 DOWNTO 0);
     SIGNAL c0_odd, c1_odd : UNSIGNED((bits/2) + 1 - 1 DOWNTO 0);
 
-    -- COMPONENT adder_generic IS
-    --     GENERIC (bits : NATURAL := bits/2);
-    --     PORT (
-    --         val1, val2 : IN STD_LOGIC_VECTOR (bits - 1 DOWNTO 0);
-    --         F          : OUT STD_LOGIC_VECTOR (bits DOWNTO 0)
-    --     );
-    -- END COMPONENT;
-
-    -- COMPONENT adder_generic_cin IS
-    --     GENERIC (bits : NATURAL := bits);
-    --     PORT (
-    --         cin        : IN STD_LOGIC;
-    --         val1, val2 : IN STD_LOGIC_VECTOR (bits - 1 DOWNTO 0);
-    --         F          : OUT STD_LOGIC_VECTOR (bits DOWNTO 0)
-    --     );
-    -- END COMPONENT;
-
 BEGIN
-
-    -- inst_LSB : adder_generic GENERIC MAP(bits/2) PORT MAP(a((BITS/2) - 1 DOWNTO 0), b((BITS/2) - 1 DOWNTO 0), c_lsb((BITS/2) DOWNTO 0));
-    -- carry_final((BITS/2) - 1 DOWNTO 0) <= c_lsb((BITS/2) - 1 DOWNTO 0); -- 3 downto 0
-    -- inst_MSB_0 : adder_generic_cin GENERIC MAP(
-    --     bits/2) PORT MAP(
-    --     '0',
-    --     a(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))),
-    --     b(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))),
-    --     c0
-    -- );
-    -- inst_MSB_1 : adder_generic_cin GENERIC MAP(
-    --     bits/2) PORT MAP(
-    --     '1',
-    --     a(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))),
-    --     b(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))),
-    --     c1
-    -- );
 
     res                                <= carry_final;
     c_lsb((BITS/2) DOWNTO 0)           <= '0' & a((BITS/2) - 1 DOWNTO 0) + b((BITS/2) - 1 DOWNTO 0);
@@ -79,18 +45,18 @@ BEGIN
         ELSE
         GENERATE
             -- When 'bits' is odd
-            --             17 -1             downto    8    (9 bits)         
-            c0_odd <= a(BITS - 1 DOWNTO BITS/2) + b(BITS - 1 DOWNTO BITS/2);
+            --             17 -1 downto 8 (9 bits)         
+            c0_odd <= a((BITS - 1) DOWNTO BITS/2) + b((BITS - 1) DOWNTO BITS/2);
             --    c1                                 <= a(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))) + b(((BITS/2) * (2)) - 1 DOWNTO ((BITS/2) * (1))) +  to_unsigned(1, b'length);
-            c1_odd <= a(BITS - 1 DOWNTO BITS/2) + b(BITS - 1 DOWNTO BITS/2) + to_unsigned(1, 1);
+            c1_odd <= a((BITS - 1) DOWNTO BITS/2) + b((BITS - 1) DOWNTO BITS/2) + to_unsigned(1, 1);
 
             IF_PROC : PROCESS (c_lsb(BITS/2), c0_odd, c1_odd) -- 4  
             BEGIN
                 IF c_lsb(BITS/2) = '0' THEN
                     -- 16 downto 8 
-                    carry_final(BITS - 1 DOWNTO BITS/2) <= c0_odd;
+                    carry_final((BITS - 1) DOWNTO BITS/2) <= c0_odd;
                     ELSE
-                    carry_final(BITS - 1 DOWNTO BITS/2) <= c1_odd;
+                    carry_final((BITS - 1) DOWNTO BITS/2) <= c1_odd;
                 END IF;
 
             END PROCESS;
